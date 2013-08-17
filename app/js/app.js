b37249a70bd0b67363746e50eb161c42f628728d
Mark Coleman <markcoleman@gmail.com>
2013-08-17 15:18:39 -0400

    updated simple service test
    Usage of $http and $httpBackend so to test the service logic.

--- app/js/app.js
+++ app/js/app.js
@@ -1,29 +1,26 @@
-/**
- * Created with JetBrains WebStorm.
- * User: colemanm
- * Date: 7/29/13
- * Time: 9:17 PM
- * To change this template use File | Settings | File Templates.
- */
-
 var app = angular.module("app", []);
 
-app.controller("SimpleCtrl", function($scope){
+app.controller("SimpleCtrl", function ($scope) {
     $scope.message = "Hello World";
 });
 
-app.factory("SimpleService", function(){
+app.factory("SimpleService", ["$http", "$q", function ($http, $q) {
 
     var service = {
+        getData: function () {
+            var deferred = $q.defer();
 
-        getData: function(){
-            return [{
-                id: 1,
-                name: "Mark"
-            }];
+            $http.get("/api/v1/fruits").success(function (data) {
+                deferred.resolve(data);
+
+            }).error(function () {
+                    deferred.reject("error");
+
+                });
+            return deferred.promise;
         }
 
     };
 
     return service;
-});
+}]);
--- test/SimpleServiceSpecs.js
+++ test/SimpleServiceSpecs.js
@@ -10,20 +10,45 @@ describe("SimpleService", function(){
 
     beforeEach(module("app"));
 
-    var service;
+    var service,
+        $httpBackend;
 
-    beforeEach(inject(function(SimpleService){
-       service = SimpleService;
+    beforeEach(inject(function(SimpleService, _$httpBackend_){
+        service = SimpleService;
+        $httpBackend = _$httpBackend_;
     }));
 
     describe("getData", function(){
 
-        it("should return an array of items", function(){
-
+        it("should make an ajax call to api/v1/fruits", function(){
+            $httpBackend.whenGET("/api/v1/fruits").respond([{id:1, name: "banana"}]);
             expect(service.getData()).toBeDefined();
-
         });
 
+        it("should resolve to an array of fruit", function(){
+            $httpBackend.whenGET("/api/v1/fruits").respond([{id:1, name: "banana"}]);
+            var promise = service.getData(),
+                theFruits = null;
+
+            promise.then(function(fruits){
+                theFruits = fruits;
+            });
+            $httpBackend.flush();
+            expect(theFruits instanceof Array).toBeTruthy();
+        });
+        it("should reject the promise and respond with error", function(){
+            $httpBackend.whenGET("/api/v1/fruits").respond(500);
+            var promise = service.getData(),
+                result = null;
+
+            promise.then(function(fruits){
+                result = fruits;
+            }, function(reason){
+                result = reason;
+            });
+            $httpBackend.flush();
+            expect(result).toBe("error");
+        });
     });
 
 });
